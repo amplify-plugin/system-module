9f16c39ef538baf227da27ccf73885b9b5512de9
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-08-28 23:17:44 +0600

    erp api module update updated

--- Base/Plugin.php
+++ Base/Plugin.php
@@ -4,4 +4,46 @@ namespace Amplify\System\Base;
 
 use Illuminate\Support\ServiceProvider;
 
-class Plugin extends ServiceProvider {}
+class Plugin extends ServiceProvider {
+
+    public function widgets(): array
+    {
+        return [];
+    }
+
+    public function navigations()
+    {
+        return [];
+    }
+
+    public function permissions()
+    {
+
+    }
+
+    public function schedules()
+    {
+        return [
+
+        ];
+    }
+
+    public function consoles(): array
+    {
+        return [];
+    }
+
+    public function routes($route) : void
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+    public function boot()
+    {
+
+    }
+}
