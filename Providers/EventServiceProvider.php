8dcaaa1a3da23814446e880115ba0cbdb6638066
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-09-05 04:03:00 +0600

    event duplicate log fixed

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -2,13 +2,13 @@
 
 namespace Amplify\System\Providers;
 
-use Amplify\System\Utility\Listeners\ApiLogListener;
-use Amplify\System\Utility\Listeners\MailLogListener;
+use Amplify\System\Listeners\QueueAfterListener;
+use Amplify\System\Listeners\QueueBeforeListener;
+use Amplify\System\Listeners\QueueFailedListener;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Http\Client\Events\ConnectionFailed;
-use Illuminate\Http\Client\Events\ResponseReceived;
-use Illuminate\Mail\Events\MessageSending;
-use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,17 +18,14 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
         protected $listen = [
-            ResponseReceived::class => [
-                ApiLogListener::class,
+            JobProcessing::class => [
+                QueueBeforeListener::class,
             ],
-            ConnectionFailed::class => [
-                ApiLogListener::class,
+            JobProcessed::class => [
+                QueueAfterListener::class,
             ],
-            MessageSending::class => [
-                MailLogListener::class,
-            ],
-            MessageSent::class => [
-                MailLogListener::class,
+            JobFailed::class => [
+                QueueFailedListener::class,
             ],
         ];
 
