bc5f058e7ce63fdbc16c3fa8ef14c4c9ac061f2b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-09-12 06:14:39 +0600

    developer option added

--- Config/amplify.php
+++ Config/amplify.php
@@ -4,9 +4,4 @@ return [
     'debug' => env('AMPLIFY_DEBUG', false),
     'client_code' => env('AMPLIFY_CLIENT_CODE', 'ACP'),
     'suppress_exception' => env('AMPLIFY_SUPPRESS_EXCEPTION', true),
-    'log_search' => false,
-    'log_payment' => false,
-    'log_erp_api' => false,
-    'log_email' => false,
-    'bug_recipient' => [],
 ];
---
+++ Config/amplify/developer.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'log_search' => false,
+    'log_payment' => false,
+    'log_erp_api' => false,
+    'log_email' => false,
+    'bug_recipient' => [],
+];
