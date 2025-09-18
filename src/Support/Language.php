bf581e9a6aa17254e073a2999edc3ab269a9f0c6
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-09-18 19:29:27 +0600

    static call added

--- src/Support/Language.php
+++ src/Support/Language.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Str;
 
 class Language extends Collection
 {
+    use \Illuminate\Support\Traits\ForwardsCalls;
+
     public function __construct()
     {
         $locales = config('backpack.crud.locales', []);
@@ -24,4 +26,34 @@ class Language extends Collection
         parent::__construct($items);
     }
 
+    /**
+     * Dynamically handle calls to the class.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     *
+     * @throws \BadMethodCallException
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        $collection = new static();
+        if (method_exists($collection, $method)) {
+            return $collection->{$method}(...$parameters);
+        }
+
+        if (! static::hasMacro($method)) {
+            throw new \BadMethodCallException(sprintf(
+                'Method %s::%s does not exist.', static::class, $method
+            ));
+        }
+
+        $macro = static::$macros[$method];
+
+        if ($macro instanceof \Closure) {
+            $macro = $macro->bindTo(null, static::class);
+        }
+
+        return $macro(...$parameters);
+    }
 }
