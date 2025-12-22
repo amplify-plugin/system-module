9aa65c93ef948169f7457126bad1ad4ee0e6d293
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-12-22 22:05:37 +0600

    product image fall back link added

--- src/Jobs/GenerateProductSlugJob.php
+++ src/Jobs/GenerateProductSlugJob.php
@@ -32,7 +32,7 @@ class GenerateProductSlugJob implements ShouldQueue
     {
         if (! empty($this->productGroups)) {
             Product::whereIn('id', $this->productGroups)->get()->each(function (Product $product) {
-                $slug = Str::limit(Str::slug($product->product_name), 75).'-'.Str::random(6);
+                $slug = Str::limit(Str::slug(strip_tags($product->product_name)), 75).'-'.Str::random(6);
                 $product->product_slug = $slug;
                 $product->save();
             });
