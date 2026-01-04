43dbf5c8646be87bc2ac9c261320bde2e56fb955
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-01-04 20:26:24 +0600

    config typo fixed

--- config/amplify.php
+++ config/amplify.php
@@ -7,7 +7,6 @@ return [
     'client_code' => env('AMPLIFY_CLIENT_CODE', 'ACP'),
     'suppress_exception' => env('AMPLIFY_SUPPRESS_EXCEPTION', true),
     'add_to_cart_pipeline' => [
-        AddToCart\ErpIsEnabled::class,
         AddToCart\DataPreparation::class,
         AddToCart\OnlyDefaultWarehouse::class,
         AddToCart\SingleWarehouseForCart::class,
