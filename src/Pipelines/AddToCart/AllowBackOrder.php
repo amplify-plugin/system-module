44c2ab262eeebd8ee8502a1d31e4d7cefae3538c
Zubayer Hossain <zubayerh4@gmail.com>
2026-03-03 17:06:06 +0600

    Added AllowBackOrder pipeline in AddToCart

--- src/Pipelines/AddToCart/AllowBackOrder.php
+++ src/Pipelines/AddToCart/AllowBackOrder.php
@@ -8,6 +8,21 @@ class AllowBackOrder implements AddToCart
 {
     public function handle(array $data, \Closure $next)
     {
+        foreach ($data['items'] as $index => $item) {
+            $quantityAvailable = (int) ($item['additional_info']['quantity_available'] ?? 0);
+
+            if ($quantityAvailable <= 0) {
+                $productAllowsBackOrder = (bool) ($item['product_back_order'] ?? false);
+                $customerAllowsBackOrder = (bool) (customer()->allow_backorder ?? false);
+
+                if (! ($productAllowsBackOrder && $customerAllowsBackOrder)) {
+                    $data['errors'][$index][] = __('The product :code is currently out of stock and back-ordering is not available.', [
+                        'code' => $item['product_code'],
+                    ]);
+                }
+            }
+        }
+
         return $next($data);
     }
 }
