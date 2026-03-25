ef97f6e352aaf547a3748f7b9d19be1175f4b228
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-03-25 06:15:11 +0600

    :pencil2: ACP => Optimization of ERP calls

---
+++ src/Traits/Overwritable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Amplify\System\Traits;
+
+trait Overwritable
+{
+    /**
+     * The registered string macros.
+     *
+     * @var array
+     */
+    protected static $overwrites = [];
+
+    /**
+     * Register a custom overwrite.
+     *
+     * @param string $name
+     * @param $overwrite
+     * @return void
+     */
+    public static function overwrite($name, $overwrite): void
+    {
+        static::$overwrites[$name] = $overwrite;
+    }
+
+    private function processOverwriteCall($method, $parameters, $instance = null)
+    {
+        $overwrite = static::$overwrites[$method];
+
+        if ($overwrite instanceof \Closure) {
+            $overwrite = $overwrite->bindTo($instance ?? $this, static::class);
+        }
+
+        return $overwrite(...$parameters);
+    }
+
+}
