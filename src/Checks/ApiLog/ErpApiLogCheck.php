fe927479628c1b8f39493dd1f858d23247ca9808
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-04-07 15:51:43 +0600

    :pencil2: ACP => Added EasyAsk and ERP API log enabled checker

---
+++ src/Checks/ApiLog/ErpApiLogCheck.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Amplify\System\Checks\ApiLog;
+
+use Amplify\System\Backend\Models\SystemConfiguration;
+use Amplify\System\Helpers\UtilityHelper;
+use Spatie\Health\Checks\Check;
+use Spatie\Health\Checks\Result;
+
+class ErpApiLogCheck extends Check
+{
+    protected bool $expected = false;
+
+    public function expectedToBe(bool $bool): self
+    {
+        $this->expected = $bool;
+
+        return $this;
+    }
+
+    public function run(): Result
+    {
+        $fileConfig = config('amplify.developer.log_erp_api', false);
+
+        $dbConfig = null;
+
+        if($entry = SystemConfiguration::whereName('developer')->whereOption('log_erp_api')->first()) {
+            $dbConfig = UtilityHelper::typeCast($entry->value, $entry->type);
+        }
+
+        $actual = $dbConfig !== null ? $dbConfig : $fileConfig;
+
+        $result = Result::make()
+            ->meta([
+                'actual' => $actual,
+                'expected' => $this->expected,
+            ])
+            ->shortSummary($this->convertToWord($actual));
+
+        return $this->expected === $actual
+            ? $result->ok()
+            : $result->failed("The ERP api log was expected to be `{$this->convertToWord((bool) $this->expected)}`, but actually was `{$this->convertToWord((bool) $actual)}`");
+    }
+
+    protected function convertToWord(bool $boolean): string
+    {
+        return $boolean ? 'true' : 'false';
+    }
+}
