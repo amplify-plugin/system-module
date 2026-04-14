14c77d1fbed8a26a3a08bcebefed707e2441c394
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-04-15 01:26:06 +0600

    :pencil2: ACP => Refactoring command keyword with class to reduce typo mistake

--- config/amplify/schedule.php
+++ config/amplify/schedule.php
@@ -13,98 +13,32 @@
 | command => variables => Command parameters at least [] required
 |
 */
+
 return [
     'logger_enabled' => true,
     'timezone' => 'UTC',
     'catalog_sync_enabled' => env('AMPLIFY_CATALOG_SYNC_ENABLED', false),
     'labels' => [
         'product_sync' => 'Catalog Synchronization',
-        'backup_run' => 'Backup Run',
-        'product_slug' => 'Generate Product Slug from Name',
-        'api_log_clean' => 'Clean API Request Log Data',
-        'audit_clean' => 'Clean Activity Log Data',
 //        'incremental-catalog' => 'Incremental Catalog Update',
         'customer-report' => 'Customer Registration Report',
         'sitemap_generate' => 'Generate Scheduled Sitemap',
     ],
     'commands' => [
         'product_sync' => [
-            'command' => 'amplify:product-sync',
+            'command' => \Amplify\ErpApi\Commands\ProductSyncCommand::class,
             'enabled' => true,
             'priority' => 3,
             'interval' => 'daily',
             'variables' => [
-                '--updatesOnly' => 'N',
-                '--processUpdates' => 'N',
+                '--updates-only' => 'N',
+                '--process-updates' => 'N',
                 '--limit' => null,
+                '--auto-update' => 'Y',
             ],
             'time' => [
                 'minute' => '0',
                 'hour' => '*',
-                'day' => '*',
-                'month' => '*',
-                'weekday' => '*',
-            ],
-        ],
-
-        'backup_run' => [
-            'command' => 'amplify:create-backup',
-            'enabled' => true,
-            'priority' => 2,
-            'interval' => 'daily',
-            'variables' => [],
-            'time' => [
-                'minute' => '0',
-                'hour' => '*',
-                'day' => '*',
-                'month' => '*',
-                'weekday' => '*',
-            ],
-        ],
-
-        'api_log_clean' => [
-            'command' => 'amplify:api-log-clean',
-            'enabled' => true,
-            'priority' => 2,
-            'interval' => 'daily',
-            'variables' => [
-                '--days' => 7,
-            ],
-            'time' => [
-                'minute' => '*',
-                'hour' => '*',
-                'day' => '*',
-                'month' => '*',
-                'weekday' => '*',
-            ],
-        ],
-
-        'audit_clean' => [
-            'command' => 'amplify:audit-clean',
-            'enabled' => true,
-            'priority' => 2,
-            'interval' => 'daily',
-            'variables' => [
-                '--days' => 30,
-            ],
-            'time' => [
-                'minute' => '*',
-                'hour' => '*',
-                'day' => '*',
-                'month' => '*',
-                'weekday' => '*',
-            ],
-        ],
-
-        'product_slug' => [
-            'command' => 'amplify:create-product-slug',
-            'enabled' => true,
-            'priority' => 2,
-            'interval' => 'cron',
-            'variables' => [],
-            'time' => [
-                'minute' => '30',
-                'hour' => '23',
                 'day' => '*',
                 'month' => '*',
                 'weekday' => '*',
@@ -143,7 +77,7 @@ return [
         ],*/
 
         'customer-report' => [
-            'command' => 'amplify:customer-registered-report',
+            'command' => \Amplify\System\Backend\Commands\CustomerRegisteredReportCommand::class,
             'enabled' => true,
             'priority' => 2,
             'interval' => 'monthly',
@@ -157,9 +91,9 @@ return [
             ],
         ],
         'sitemap_generate' => [
-            'command' => 'amplify:sitemap-generate',
+            'command' => \Amplify\System\Commands\SitemapGenerateCommand::class,
             'enabled' => true,
-            'priority' => 2,
+            'priority' => 10,
             'interval' => 'weekly',
             'variables' => [],
             'time' => [
