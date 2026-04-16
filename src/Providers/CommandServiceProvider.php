39e2736e7bfac6d4078982204ee8e7370979dee1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-04-16 23:14:47 +0600

    :pencil2: ACP => Added a command to run analyze and optimize db to ensure table length are accurate for health check

--- src/Providers/CommandServiceProvider.php
+++ src/Providers/CommandServiceProvider.php
@@ -4,6 +4,7 @@ namespace Amplify\System\Providers;
 
 use Amplify\System\Commands\AddProductThumbnailCommand;
 use Amplify\System\Commands\CreateAllLoginCommand;
+use Amplify\System\Commands\DefragmentTablesCommand;
 use Amplify\System\Commands\DeleteProductsCommand;
 use Amplify\System\Commands\EasyAskDatabaseExportCommand;
 use Amplify\System\Commands\FetchTracePartsCatalogCommand;
@@ -34,7 +35,44 @@ class CommandServiceProvider extends ServiceProvider
                 TransformProduct::class,
                 AddProductThumbnailCommand::class,
                 SitemapGenerateCommand::class,
+                DefragmentTablesCommand::class
             ]);
+        }
+    }
+
+    private function registerScheduler()
+    {
+        if (config('app.env') === 'production' && $this->app->runningInConsole()) {
+            $this->app->booted(function () {
+                $schedule = app(\Illuminate\Console\Scheduling\Schedule::class);
+
+                if (config('amplify.easyask_sftp_export', false)) {
+                    $schedule->command(EasyAskDatabaseExportCommand::class, [
+                        'tableList' => 'attribute_product_classification,attribute_product,attribute_values,'
+                            . 'attributes,categories,category_product,customer_group_product,customer_groups,'
+                            . 'customers,manufacturers,option_product_classification,option_product,'
+                            . 'options,products,product__images,products,warehouses'
+                    ])
+                        ->timezone(\config('amplify.schedule.timezone', \config('app.timezone', 'UTC')))
+                        ->daily()
+                        ->withoutOverlapping()
+                        ->onOneServer();
+                }
+
+                $schedule->command('queue:prune-batches', ['--quiet' => true])
+                    ->timezone(\config('amplify.schedule.timezone', \config('app.timezone', 'UTC')))
+                    ->daily();
+
+                $schedule->command(DefragmentTablesCommand::class, ['--analyze' => true])
+                    ->timezone(\config('amplify.schedule.timezone', \config('app.timezone', 'UTC')))
+                    ->dailyAt('03:00');
+
+                $schedule->command(DefragmentTablesCommand::class, ['--optimize' => true])
+                    ->timezone(\config('amplify.schedule.timezone', \config('app.timezone', 'UTC')))
+                    ->saturdays()->at('05:00')
+                    ->withoutOverlapping()
+                    ->onOneServer();
+            });
         }
     }
 }
