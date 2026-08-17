8af014f188eaa1148ac13a3dbfa1a361ef605a7d
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-08-17 16:11:22 +0600

    self health check api added

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Amplify\System\Http\Api\Controllers\ContactFindController;
 use Illuminate\Support\Facades\Route;
 use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
 
-Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
+Route::group(['middleware' => ['api'], 'prefix' => 'admin/api'], function () {
 
-    Route::get('health', HealthCheckJsonResultsController::class);
+    Route::get('health', HealthCheckJsonResultsController::class)->name('admin.api.health');
 
     if (config('amplify.api.contact_detail', false)) {
         Route::get('contacts/{contact_code}', ContactFindController::class)
