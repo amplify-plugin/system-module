f25344da00335d8388f481e952162c4a00a8baa7
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-09-08 01:35:16 +0600

    Pro trait replacement added

--- src/Marketing/Http/Controllers/MerchandisingZoneCrudController.php
+++ src/Marketing/Http/Controllers/MerchandisingZoneCrudController.php
@@ -12,7 +12,7 @@ use Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 use Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanel;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
-use Backpack\Pro\Http\Controllers\Operations\FetchOperation;
+use Backpack\CRUD\app\Http\Controllers\Operations\FetchOperation;
 use Illuminate\Http\JsonResponse;
 
 /**
